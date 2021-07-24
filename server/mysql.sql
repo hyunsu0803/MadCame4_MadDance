b80b33d79c87e5ea7434e7cda96b761d2afe5deb
Hyemin Lee <lhmin0614@gmail.com>
2021-07-24 05:30:09 +0000

    server get score

--- server/mysql.sql
+++ server/mysql.sql
@@ -1,20 +1,26 @@
 -- create database MadDance DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 -- use MadDance;
 
--- CREATE TABLE brd1 (
---     UserID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     UserName CHAR(25) NOT NULL,
---     UserSore INT(10) NOT NULL
--- );
+CREATE TABLE brd1 (
+    UserID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    UserName CHAR(25) NOT NULL,
+    UserScore INT(10) NOT NULL
+);
 
--- CREATE TABLE brd2 (
---     UserID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     UserName CHAR(25) NOT NULL,
---     UserSore INT(10) NOT NULL
--- );
+CREATE TABLE brd2 (
+    UserID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    UserName CHAR(25) NOT NULL,
+    UserScore INT(10) NOT NULL
+);
 
--- CREATE TABLE brd3 (
---     UserID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     UserName CHAR(25) NOT NULL,
---     UserSore INT(10) NOT NULL
--- );
+CREATE TABLE brd3 (
+    UserID INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    UserName CHAR(25) NOT NULL,
+    UserScore INT(10) NOT NULL
+);
+
+
+INSERT INTO brd1 (UserName, UserScore) VALUES("hyemin", 90);
+INSERT INTO brd1 (UserName, UserScore) VALUES("hyemin", 91);
+INSERT INTO brd1 (UserName, UserScore) VALUES("hyemin", 88);
+INSERT INTO brd1 (UserName, UserScore) VALUES("hyemin", 70);
